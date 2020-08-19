2ccb5388dce774bfd78f486c842a9ab99ebc962b
Jennifer Pazos <jmp521@gmail.com>
2020-08-19 00:09:03 -0500

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,15 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    console.log("props", this.props)
+    const filteredReviews = this.props.reviews.filter(r => r.restaurantId === this.props.restaurantId)
+    const matchedReviews = filteredReviews.map(review => <Review key={review.id}  review={review} deleteReview={this.props.deleteReview} />)
+   
+    
     return (
       <ul>
-        Reviews
+        
+        {matchedReviews}
       </ul>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,34 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+    reviews: [], 
+    restaurants: []
 
+}, action) {
+    
+
+    switch(action.type) {
+        case "ADD_RESTAURANT":
+            // console.log("inside add_restaurant", action)
+            // return {...state, restaurants: state.restaurants.concat({text: action.text, id: cuidFn() })}
+            const restaurant = { text: action.text, id: cuidFn() };
+      return {
+        ...state,
+        restaurants: [ ...state.restaurants, restaurant]
+      }
+
+        case "DELETE_RESTAURANT":
+            return {...state, restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id)}
+        
+        case "ADD_REVIEW": 
+        console.log("action", action.review.text)
+            const review = { text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn() };
+            return {...state, reviews: [...state.reviews, review]}
+
+        case "DELETE_REVIEW":
+            return {...state, reviews: state.reviews.filter(review => review.id !== action.id)}
+        default: 
+            return state;
+    }
 }
