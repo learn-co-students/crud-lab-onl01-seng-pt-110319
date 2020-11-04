f330941734742c17b0fd91fec6c8e65c2184aaf0
Mug <brad.alexander913@gmail.com>
2020-11-03 19:43:43 -0800

    setup restaurantinput

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class RestaurantInput extends Component {
+  state={
+    text: ""
+  }
+  
+  handleOnChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    //this.props.
+  }
+
   render() {
     return (
       <div>
         Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          Text: <input type="text" name="text" onChange={this.handleOnChange}/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
 };
 
-export default RestaurantInput;
+export default connect() (RestaurantInput);
